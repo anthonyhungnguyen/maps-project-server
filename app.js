3bec331befa7d9087337642cec2252e10f2d6265
Anthony Hung Nguyen <anthonyhungnguyen276@gmail.com>
2020-06-06 14:54:45 +0700

    second release

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ wsServer.on('request', function(request) {
 	connection.on('message', function(message) {
 		const { utf8Data } = message
 		const { msg } = JSON.parse(utf8Data)
-		publish_client.publish('/light', msg)
+		publish_client.publish('/light', 'HELLOOOOOO')
 	})
 
 	connection.on('close', function(connection) {
